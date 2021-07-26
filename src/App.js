57bfa4b96d2f4971e14d27129ade8092aab56f0c
piyushsinghhsv <piyush.singh@hsv.digital>
2021-07-27 03:11:36 +0530

    added login page and drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import Button from "@material-ui/core/Button";
+import Sidebar from "./components/sidebar";
+import Login from "./container/login";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.background.main,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
+    <div className="App" className={classes.root}>
+      <Login />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: "#e4f0e2",
+    },
+  },
+});
+
+const themeDark = createTheme({
+  palette: {
+    primary:{
+      main:"rgb(255,255,0)"
+    },
+    background: {
+      main: "#222222",
+    },
+    text: {
+      primary: "#ffffff",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeDark}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
