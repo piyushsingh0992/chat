2271698263062edb4d41e32650770415a26db21b
piyushsinghhsv <piyush.singh@hsv.digital>
2021-07-27 11:29:08 +0530

    cleared the whole app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
-import Button from "@material-ui/core/Button";
-import Sidebar from "./components/sidebar";
-import Login from "./container/login";
-import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    background: theme.palette.background.main,
-  },
-}));
+
 
 function App() {
-  const classes = useStyles();
   return (
-    <div className="App" className={classes.root}>
-      <Login />
+    <div className="App" >
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,34 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
-const themeLight = createTheme({
-  palette: {
-    background: {
-      default: "#e4f0e2",
-    },
-  },
-});
-
-const themeDark = createTheme({
-  palette: {
-    primary:{
-      main:"rgb(255,255,0)"
-    },
-    background: {
-      main: "#222222",
-    },
-    text: {
-      primary: "#ffffff",
-    },
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={themeDark}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
